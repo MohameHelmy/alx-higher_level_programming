ad76f2e9d7d48e9aeef8e06da1d60cef71c28cf7
MohameHelmy <Mohamed.helmy11d@gmail.com>
2023-11-21 12:32:54 -0800

    a

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This is a module-level docstring for the square module.
+
+You can provide an overview of the module, its purpose, and any important
+information or usage instructions.
+
+- Version: 1.0.0
+- Date: June 30, 2023
+- License: MIT License
+
+"""
+
+
+class Square:
+    """
+    This is a Square class representing a square shape.
+
+    You can provide more information about the class, its attributes, methods,
+    and usage instructions.
+
+    """
+    pass
